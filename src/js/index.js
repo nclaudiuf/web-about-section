76132a6ee9b1882325b3fbb5413e15ff23d2a4f0
Claudiu <12200607+nclaudiuf@users.noreply.github.com>
2021-08-30 12:06:56 +0700

    major updates

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,9 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {jarallax, jarallaxElement} from 'jarallax'; //Wiki & Credits: https://github.com/nk-o/jarallax
 import 'aos';
 
 init();
-// Add Element Extension.
-jarallaxElement();
-// Add Jarallax on DOM Node.
-jarallax(document.querySelectorAll('[data-jarallax-element]'));
 
 function init() {
 	window.$ = $;
